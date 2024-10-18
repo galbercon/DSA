8373e716906c984a2200b8f4c3bcb910c4eb4246
galbercon <95376225+galbercon@users.noreply.github.com>
2024-10-18 12:16:24 +0200

    Add files via upload

---
+++ vaja2/merjenje_temperature.ino
@@ -0,0 +1,36 @@
+/*
+  AnalogReadSerial
+
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+*/
+float izracunTemp (int vrednost)
+{
+  float rezultat;
+  rezultat = (vrednost/1023.0*5000.0-400.0)/19.53;
+  return rezultat;
+}
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A1);
+  // print out the value you read:
+  //Serial.println(sensorValue);
+  //float tempNap = (sensorValue/1023.0)*5000.0; //pretvorba v mV
+  //Serial.println(tempNap);
+  //float temperatura = (tempNap-400.0)/19.53; //mV v stopinje Celzija
+  float temperatura = izracunTemp(sensorValue);
+  Serial.println(temperatura);
+  delay(200);  // delay in between reads for stability
+}
